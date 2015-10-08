15c6d286b539b1bd264869bc403f34b853b432ed
Dan K <itsdanya@gmail.com>
2015-10-08 15:24:10 +0300

    Create seeds

--- Gemfile
+++ Gemfile
@@ -32,5 +32,6 @@ group :development do
   gem 'web-console', '~> 2.0'
   gem 'spring'
   gem 'annotate'
+  gem 'ffaker'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.create!(
+  first_name: 'Foo', last_name: 'Bar', email: 'foo@bar.com',
+  phone_number: '+75551234567', password: 'foobar'
+)
+u.update!(api_token: 'foobar')
+
+# Ingredients
+20.times do
+  Ingredient.create!(
+    name: FFaker::Food.ingredient,
+    remote_image_url: 'http://lorempixel.com/150/150/food/'
+  )
+end
+
+# Days & schedules
+Date::DAYNAMES.each_with_index do |dayname, i|
+  day = Day.create!(day_of_week: i)
+
+  5.times do
+    dish = day.dishes.create!(
+      name: FFaker::Food.vegetable,
+      description: FFaker::BaconIpsum.sentence,
+      price: rand(500) + 1,
+      remote_image_url: 'http://lorempixel.com/450/800/food/'
+    )
+    dish.ingredients << Ingredient.order('RANDOM()').limit(5)
+  end
+end
+
+# Lunches
+5.times do
+  dish = Dish.create!(
+    name: FFaker::Food.vegetable,
+    description: FFaker::BaconIpsum.sentence,
+    price: rand(500) + 1,
+    remote_image_url: 'http://lorempixel.com/450/800/food/',
+    lunch: true
+  )
+  dish.ingredients << Ingredient.order('RANDOM()').limit(5)
+end
+
+# Stuff
+5.times do
+  Dish.create!(
+    name: FFaker::Food.vegetable,
+    description: FFaker::BaconIpsum.sentence,
+    price: rand(500) + 1,
+    remote_image_url: 'http://lorempixel.com/450/800/food/',
+    stuff: true
+  )
+end
