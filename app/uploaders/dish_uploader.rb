2747d92d4c85eda8f281353c67d7b9cb5833ca7b
Dan K <itsdanya@gmail.com>
2015-10-08 15:24:45 +0300

    Set up image compression, ignore uploads

--- app/uploaders/dish_uploader.rb
+++ app/uploaders/dish_uploader.rb
@@ -1,6 +1,6 @@
 class DishUploader < ApplicationUploader
   # Process files as they are uploaded:
-  process resize_to_limit: [240, 720]
+  process resize_to_limit: [450, 800]
   process convert: 'jpg'
   process quality: 85
 end
