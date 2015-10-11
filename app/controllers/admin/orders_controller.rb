5a3e9582334b0cd4d09927640ab02da0eb9eba26
Dan K <itsdanya@gmail.com>
2015-10-11 15:11:44 +0300

    Admin orders

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,50 @@
+class Admin::OrdersController < Admin::AdminController
+  before_action :set_admin_order, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def edit
+  end
+
+  def create
+    @order = Order.new(admin_order_params)
+
+    if @order.save
+      redirect_to [:admin, @order], notice: 'Order was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @order.update(admin_order_params)
+      redirect_to [:admin, @order], notice: 'Order was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @order.destroy
+    redirect_to admin_orders_url, notice: 'Order was successfully destroyed.'
+  end
+
+  private
+
+  def set_admin_order
+    @order = Order.find(params[:id])
+  end
+
+  def admin_order_params
+    params.require(:order).permit(:address)
+  end
+end
