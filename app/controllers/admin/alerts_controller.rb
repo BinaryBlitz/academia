94e5d4c400eda4fb828f48361818612a1e7726c1
Dan K <itsdanya@gmail.com>
2015-12-16 18:26:07 +0300

    Order orders in alerts by date ascending

--- app/controllers/admin/alerts_controller.rb
+++ app/controllers/admin/alerts_controller.rb
@@ -1,7 +1,7 @@
 class Admin::AlertsController < Admin::AdminController
   def index
     @menu_present = Day.present_for_the_next_three_days?
-    @unassigned = Order.unassigned
-    @late = Order.late
+    @unassigned = Order.unassigned.order(created_at: :asc)
+    @late = Order.late.order(created_at: :asc)
   end
 end
