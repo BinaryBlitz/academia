09452d07722dc6def238229b8a52ea860fe67141
Dan K <itsdanya@gmail.com>
2015-12-16 15:02:15 +0300

    Change unassigned orders behaviour

--- app/models/order.rb
+++ app/models/order.rb
@@ -48,7 +48,7 @@ class Order < ActiveRecord::Base
   scope :delivered, -> { where(status: :delivered) }
   scope :rejected, -> { where(status: :rejected) }
   scope :on_the_way, -> { where(status: :on_the_way) }
-  scope :unassigned, -> { on_the_way.where(courier: nil) }
+  scope :unassigned, -> { where(status: :new).where(courier: nil) }
   scope :late, -> { on_the_way.where('created_at < ?', MAX_DELIVERY_MINUTES.minutes.ago) }
 
   def total_price
