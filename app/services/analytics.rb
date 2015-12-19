b491a3caceddfd4deeedffc51890bda9fe62533e
Dan K <itsdanya@gmail.com>
2015-12-19 13:46:29 +0300

    Render stats increase

--- app/services/analytics.rb
+++ app/services/analytics.rb
@@ -6,33 +6,35 @@ class Analytics
   end
 
   def new_orders
+    return @new_orders if @new_orders
     users = User.joins(:orders).group('users.id').having('COUNT(orders.id) = ?', 1)
-    @orders.where(user: users).count
+    @new_orders = @orders.where(user: users).count
   end
 
   def repeated_orders
+    return @repeated_orders if @repeated_orders
     users = User.joins(:orders).group('users.id').having('COUNT(orders.id) > ?', 1)
-    @orders.where(user: users).count
+    @repeated_orders = @orders.where(user: users).count
   end
 
   def total_orders
-    @orders.count
+    @total_orders ||= @orders.count
   end
 
   def revenue
-    @orders.sum(:revenue)
+    @revenue ||= @orders.sum(:revenue)
   end
 
   def discount
-    @orders.sum(:discount)
+    @discount ||= @orders.sum(:discount)
   end
 
   def balance_discount
-    @orders.sum(:balance_discount)
+    @balance_discount ||= @orders.sum(:balance_discount)
   end
 
   def total_sum
-    revenue + discount + balance_discount
+    @total_sum ||= revenue + discount + balance_discount
   end
 
   def average_sum
@@ -41,14 +43,16 @@ class Analytics
   end
 
   def average_delivery_time
+    return @average_delivery_time if @average_delivery_time
     delivered_orders = @orders.delivered.pluck(:created_at, :delivered_at)
     return 0 if delivered_orders.count == 0
-    delivered_orders.map { |order| order[1] - order[0] }.sum / delivered_orders.count
+    @average_delivery_time = delivered_orders.map { |order| order[1] - order[0] }.sum / delivered_orders.count
   end
 
   def repeated_orders_for_unique_users
+    return @repeated_orders_for_unique_users if @repeated_orders_for_unique_users
     return if User.count == 0
     users = User.joins(:orders).group('users.id').having('COUNT(orders.id) > ?', 2)
-    (users.count.size.to_f / User.count) * 100
+    @repeated_orders_for_unique_users = (users.count.size.to_f / User.count) * 100
   end
 end
