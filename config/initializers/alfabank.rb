b0d8118c23ff8ccd31bc15678bf72ded0e14fbdf
Dan K <itsdanya@gmail.com>
2016-02-23 02:34:17 +0300

    Clean up alfabank initializer

--- config/initializers/alfabank.rb
+++ config/initializers/alfabank.rb
@@ -3,7 +3,6 @@ Alfabank.setup do |config|
   config.password = 'zdorovoepitanie'
   config.language = 'ru'
   config.return_url = 'finish.html'
-  config.error_url = 'error.html'
   config.currency = Alfabank::Currency::RUB
   config.order_number_prefix = 'test-payment-'
   config.binding_username = 'zdorovoepitanie_auto-api'
