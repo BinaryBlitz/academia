f935fd4b9389ecb1bdc7cdfb39b5db1b05478c82
Dan K <itsdanya@gmail.com>
2016-09-11 02:54:56 +0300

    Uninstall Capistrano

--- config/deploy/production.rb
+++
@@ -1,3 +0,0 @@
-server 'zpapp@128.199.51.211', :app, :web, :db, primary: true
-set :branch, 'master'
-set :rails_env, 'production'
--- config/deploy/staging.rb
+++
@@ -1,3 +0,0 @@
-server 'zpapp@binaryblitz.ru', :app, :web, :db, primary: true
-set :branch, 'development'
-set :rails_env, 'staging'
