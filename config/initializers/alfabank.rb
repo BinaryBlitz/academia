0497271388e6ef5a6928eb17486276bda10baa33
Dan Kim <itsdanya@gmail.com>
2016-10-25 00:52:50 +0300

    Alfabank: (╯°□°）╯︵ ┻━┻

--- config/initializers/alfabank.rb
+++ config/initializers/alfabank.rb
@@ -7,7 +7,7 @@ Alfabank.setup do |config|
   config.order_number_prefix = 'test-payment-'
   config.binding_username = Rails.application.secrets.alfabank_binding_username
   config.binding_password = Rails.application.secrets.alfabank_binding_password
-  config.mode = Rails.application.secrets.alfabank_mode || :test
+  config.mode = (Rails.application.secrets.alfabank_mode == 'production' ? :production : :test)
 end
 
 Alfabank.logger = Rails.logger
