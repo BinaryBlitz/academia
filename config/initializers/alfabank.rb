1565b5f9eb26001e8b849300be3c5d729d303ef0
Dan Kim <itsdanya@gmail.com>
2016-10-25 15:49:19 +0300

    Alfabank: add return_url to secrets

--- config/initializers/alfabank.rb
+++ config/initializers/alfabank.rb
@@ -2,7 +2,7 @@ Alfabank.setup do |config|
   config.username = Rails.application.secrets.alfabank_username
   config.password = Rails.application.secrets.alfabank_password
   config.language = 'ru'
-  config.return_url = 'finish.html'
+  config.return_url = Rails.application.secrets.alfabank_return_url
   config.currency = Alfabank::Currency::RUB
   config.order_number_prefix = 'test-payment-'
   config.binding_username = Rails.application.secrets.alfabank_binding_username
