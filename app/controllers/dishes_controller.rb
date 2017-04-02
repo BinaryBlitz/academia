70f679a5aba8708df45e56cc3ac06392bf4d9769
SumLare <mrhard95@gmail.com>
2017-04-02 15:59:04 +0300

    API: add category to dishes

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,11 +1,19 @@
 class DishesController < ApplicationController
+  before_action :set_category, only: [:index, :stuff]
+
   def index
     @day = Store.new.today
-    @dishes = @day.dishes
+    @dishes = @day.dishes.where(category: @category)
   end
 
   def stuff
-    @dishes = Dish.where(stuff: true).visible
+    @dishes = @category.dishes.where(stuff: true).visible
     render :index
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
   end
 end
