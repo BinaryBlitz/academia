5957462809bba35b710cfcf91cd840f60064308e
SumLare <mrhard95@gmail.com>
2017-04-14 01:12:47 +0300

    Remove scope with schedules on day in dish

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -54,8 +54,6 @@ class Dish < ActiveRecord::Base
   mount_uploader :image, DishUploader
 
   scope :visible, -> { where(hidden: false) }
-  scope :visible, -> { where(hidden: false) }
-  scope :with_schedules_on_day, -> (day) { includes(:schedules).where('schedules.day': day) }
 
   def has_nutrition_info?
     proteins || fats || carbohydrates || calories
